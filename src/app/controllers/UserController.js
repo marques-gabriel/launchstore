9c199a80b67b6e942c9f9b34ebea6efb5583586b
Gabriel Marques <marquesgabriel@live.com>
2021-01-24 20:02:25 -0300

    Cascade delete

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -58,6 +58,24 @@ module.exports = {
             })
         }
 
+    },
+
+    async delete(req, res) {
+        try {
+            await User.delete(req.body.id)
+            req.session.destroy()
+
+            return res.render("session/login", {
+                success: "Conta deletada com sucesso!"
+            })
+
+        } catch (error) {
+            console.error(err)
+            return res.render("user/index", {
+                user: req.body,
+                error: "Erro ao deletar a conta"
+            })
+        }
     }
 
 }
--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,6 +1,7 @@
 const db = require('../../config/db')
 const { hash } = require('bcryptjs')
-// const { update } = require('../controllers/UserController')
+const Product =  require('../models/Product')
+const fs = require('fs')
 
 module.exports = {
     async findOne(filters) {
@@ -80,5 +81,34 @@ module.exports = {
 
         await db.query(query)
         return
+    },
+
+    async delete(id) {
+        // pegar todos os produtos 
+        let results = await db.query('SELECT * FROM products WHERE user_id = $1', [id])
+        const products =  results.rows
+
+        // pegar todas imagens dos produtos 
+        const allFilesPromise = products.map(product => 
+            Product.files(product.id)
+        )
+
+        let promiseResults = await Promise.all(allFilesPromise)
+
+        // rodar a remocao do usuario 
+        await db.query('DELETE FROM users WHERE id = $1', [id])
+
+        // remover imagens da pasta public 
+        promiseResults.map(results => {
+            results.rows.map(file => {
+
+                try {
+                    fs.unlinkSync(file.path)
+                } catch (err) {
+                    console.log(err)
+                }
+            })
+        })
+
     }
 }

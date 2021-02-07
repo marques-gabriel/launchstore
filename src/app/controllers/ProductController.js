5eddd42781a31615394d1ad7b4443c66ecf4df0f
Gabriel Marques <marquesgabriel@live.com>
2021-02-07 16:27:14 -0300

    Product validations

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -23,17 +23,6 @@ module.exports = {
     async post(req, res) {
 
         try {
-            const keys= Object.keys(req.body)
-
-            for(key of keys) {
-                if (req.body[key] == "")
-                    return res.send("Por favor, preencha todos os campos!")
-            }
-
-
-            if (req.files.length == 0)
-                return res.send("Please, send at least one image")
-
             
             let { category_id, name, description, old_price, price, quantity, status } = req.body
 
@@ -103,14 +92,6 @@ module.exports = {
     async put(req, res) {
 
         try {
-
-            const keys= Object.keys(req.body)
-
-            for(key of keys) {
-                if (req.body[key] == "" && key != "removed_files") {
-                    return res.send("Por favor, preencha todos os campos!")
-                }
-            }
 
             if (req.files.length != 0) {
                 const newFilesPromise = req.files.map(file => 

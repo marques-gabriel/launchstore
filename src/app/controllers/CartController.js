24c91f7f1994051a599cf93cade176376680d318
Gabriel Marques <marquesgabriel@live.com>
2021-02-16 20:02:14 -0300

    Cart html structure

---
+++ src/app/controllers/CartController.js
@@ -0,0 +1,22 @@
+const Cart = require('../../lib/cart')
+
+module.exports = {
+
+    index(req, res) {
+
+        try {
+
+            let { cart } = req.session
+
+            // gerenciador de carrinho
+            cart = Cart.init(cart)
+
+            return res.render("cart/index", { cart })
+
+        } catch (error) {
+
+            console.error(error)
+        }
+
+    }
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ const HomeController = require('../app/controllers/HomeController')
 
 const products = require('./products')
 const users = require('./users')
+const cart = require('./cart')
 
 
 
@@ -14,6 +15,7 @@ routes.get('/', HomeController.index)
 
 routes.use('/products', products)
 routes.use('/users', users)
+routes.use('/cart', cart)
 
 // Alias
 routes.get('/ads/create', function(req, res) {

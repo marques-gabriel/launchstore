857ac18d3bfdc12a44e9a25d8ff427d4cafc5d36
Gabriel Marques <marquesgabriel@live.com>
2021-02-19 12:36:43 -0300

    Change orders status

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -5,6 +5,7 @@ const Order = require('../models/Order')
 
 const mailer = require('../../lib/mailer')
 const Cart = require('../../lib/cart')
+const { update } = require('../models/Order')
 
 const email = (seller, product, buyer) =>`
     <h2>Olá ${seller.name}</h2>
@@ -122,5 +123,43 @@ module.exports = {
             return res.render('orders/error')
         }
 
+    },
+
+    async update(req, res) {
+        try {
+            
+            const { id, action } = req.params
+
+            const acceptedActions = ['close', 'cancel']
+            if(!acceptedActions.includes(action)) return res.send("Can't do this action")
+
+            // pegar o pedido
+            const order = await Order.findOne({
+                where: { id }
+            })
+
+            if(!order) return res.send('Order not found')
+
+            // verificar se está aberto
+            if(order.status != 'open') return res.send("Can't do this action (status is not open)")
+
+            // atualizar o pedido
+            const statuses = {
+                close: "sold",
+                cancel: "canceled",
+            }
+
+            order.status =  statuses[action]
+
+            await Order.update(id, {
+                status: order.status
+            })
+
+            // redirecionar
+            return res.redirect('/orders/sales')
+
+        } catch (error) {
+            console.error(error)
+        }
     }
 }
--- src/routes/orders.js
+++ src/routes/orders.js
@@ -9,6 +9,7 @@ routes.post('/', onlyUsers, OrderController.post)
     .get('/', onlyUsers, OrderController.index)
     .get('/sales', onlyUsers, OrderController.sales)
     .get('/:id', onlyUsers, OrderController.show)
+    .post('/:id/:action', onlyUsers, OrderController.update)
 
 
 
